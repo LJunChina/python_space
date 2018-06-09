c378c815279883b6f65f296522f2fac04bfd97c6
Jon_China <503665965@qq.com>
2018-06-09 16:45:19 +0800

    feat(var):变量使用

--- demo.py
+++ demo.py
@@ -9,5 +9,15 @@ else:
 地方斯蒂芬几十块大数据分开了时代峻峰
 第三方士大夫水电费水电费
 '''
+# 以下代码用来打印
 print('a'),
 print('b')
+
+# 变量的定义
+score = 100  # 定义了一个变量 里面存储了一个数值 为100
+high = 180  # 单位是cm
+applePrice = 3.5
+weight = 7.5
+print(applePrice * weight)  # 如果出现第二次 变量 = ？表示赋值
+
+
